3f103ffcc07cffe50b8308446bee11c6b66a87a5
dallenb4 <dallenbryce4@outlook.com>
2025-03-13 15:22:35 -0700

    Removed redundant index.html file

--- src/init.js
+++ src/init.js
@@ -1,4 +1,2 @@
-// This file doesn't appear in the build, only used for dev
-
-window.global = window
-import * as RPGM from './init.ts'
+/** @file This file doesn't appear in the build, only used for dev */
+import './init.ts'
