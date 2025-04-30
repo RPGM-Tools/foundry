e5cc3f7c97bb2926fa4771bf09fa0cb23d8fb217
dallenb4 <dallenbryce4@outlook.com>
2025-04-30 16:56:46 -0700

    Fixed github workflows

--- modules/rpgm-forge/vite.config.ts
+++ modules/rpgm-forge/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
-import { version } from './package.json';
 import defaultConfig from '../../shared/vite/vite.config';
 
 export default defineConfig(({ mode }) => {
-	return defaultConfig('rpgm-forge', mode, __dirname, version);
+	return defaultConfig('rpgm-forge', mode, __dirname);
 });
--- modules/rpgm-vault/vite.config.ts
+++ modules/rpgm-vault/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
-import { version } from './package.json';
 import defaultConfig from '../../shared/vite/vite.config';
 
 export default defineConfig(({ mode }) => {
-	return defaultConfig('rpgm-vault', mode, __dirname, version);
+	return defaultConfig('rpgm-vault', mode, __dirname);
 });
