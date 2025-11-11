8345ec93d3f7119bc6d1be692d1f0515e1e37a3f
Aaron Westover <61663569+neostryder@users.noreply.github.com>
2025-11-11 00:48:55 -0700

    fix(chat) Lil fix

--- modules/rpgm-forge/src/forge.ts
+++ modules/rpgm-forge/src/forge.ts
@@ -20,20 +20,35 @@ import HomebrewChat from '$/chat/Homebrew/HomebrewChat.vue';
 import InitPrompt from '$/chat/InitPrompt.vue';
 import NamesChat from '$/chat/NamesChat.vue';
 import { command } from '$/util/homebrew';
-import { chatDescription, chatTokenNames, getSelectedToken, quickNameToken, registerTokenCreate } from '$/util/token';
+import {
+	chatDescription,
+	chatTokenNames,
+	getSelectedToken,
+	quickNameToken,
+	registerTokenCreate
+} from '$/util/token';
 import Genres from '$$/assets/combined_systems.json?url';
 
 import ForgeSidebar from './sidebar/ForgeSidebar.vue';
 
-export class RpgmForge extends FoundyRpgmModuleMixin<typeof AbstractForge, AbstractForge.Settings>(AbstractForge) {
+export class RpgmForge extends FoundyRpgmModuleMixin<
+	typeof AbstractForge,
+	AbstractForge.Settings
+>(AbstractForge) {
 	/** @returns The current genre setting */
-	get genre() { return game.settings.get('rpgm-forge', 'genre'); }
+	get genre() {
+		return game.settings.get('rpgm-forge', 'genre');
+	}
 
 	/** @returns The current system setting */
-	get system() { return game.system.title; }
+	get system() {
+		return game.system.title;
+	}
 
 	/** @returns The current language setting */
-	get language() { return game.settings.get('rpgm-forge', 'language'); }
+	get language() {
+		return game.settings.get('rpgm-forge', 'language');
+	}
 
 	homebrewSchemas: HomebrewSchema[] = [];
 	genres: Partial<Record<string, { genre: string }>> = {};
@@ -47,7 +62,7 @@ export class RpgmForge extends FoundyRpgmModuleMixin<typeof AbstractForge, Abstr
 		rpgm.forge = this;
 		this.homebrewSchemas = HomebrewSchemas as HomebrewSchema[];
 		const genresUrl = new URL(Genres, import.meta.url);
-		this.genres = await (await fetch(genresUrl)).json() as typeof this.genres;
+		this.genres = (await (await fetch(genresUrl)).json()) as typeof this.genres;
 		this.promptChats = new ChatWizard(
 			this.id,
 			'prompt',
@@ -127,35 +142,55 @@ export class RpgmForge extends FoundyRpgmModuleMixin<typeof AbstractForge, Abstr
 			type: String,
 			config: true
 		});
-		rpgm.chat.registerCommand(literal('name')
-			.then(argument('prompt', string('greedy_phrase')).executes(c => {
-				void chatTokenNames(undefined, c.get<string>('prompt'));
-			})).executes(() => {
-				void chatTokenNames(undefined);
-			}));
-		rpgm.chat.registerCommand(literal('description')
-			.then(argument('prompt', string('greedy_phrase')).executes(c => {
-				void chatDescription({ type: c.get<string>('prompt')! });
-			})).executes(() => {
-				const token = getSelectedToken();
-				if (!token) return;
-				chatDescription(
-					{
+		rpgm.chat.registerCommand(
+			literal('name')
+				.then(
+					argument('prompt', string('greedy_phrase')).executes(c => {
+						void chatTokenNames(undefined, c.get<string>('prompt'));
+					})
+				)
+				.executes(() => {
+					void chatTokenNames(undefined);
+				})
+		);
+		rpgm.chat.registerCommand(
+			literal('description')
+				.then(
+					argument('prompt', string('greedy_phrase')).executes(c => {
+						void chatDescription({ type: c.get<string>('prompt')! });
+					})
+				)
+				.executes(() => {
+					const token = getSelectedToken();
+					if (!token) return;
+					chatDescription({
 						type: token.actor!.prototypeToken.name,
 						// Don't include name if it's the default actor name
-						name: token.name ? token.name !== token.actor!.prototypeToken.name ? token.name : '' : ''
+						name: token.name
+							? token.name !== token.actor!.prototypeToken.name
+								? token.name
+								: ''
+							: ''
 					});
-			}));
+				})
+		);
 		command();
-		rpgm.radialMenu.registerCategory('rpgm_forge', { color: '217deg', logger: this.logger });
+		rpgm.radialMenu.registerCategory('rpgm_forge', {
+			color: '217deg',
+			logger: this.logger
+		});
 		rpgm.radialMenu.registerInputButton({
 			category: rpgm.radialMenu.categories.rpgm_forge,
 			icon: 'fa fa-dice-d4',
 			tooltip: 'RPGM_FORGE.RADIAL_MENU.D4',
-			detective: (context) => inputHeuristics(context).noNumber().result,
-			callback: async (context) => {
+			detective: context => inputHeuristics(context).noNumber().result,
+			callback: async context => {
 				const shimmer = shimmerInput(context);
-				await writeOn(context, `Rolled a ${Math.floor(Math.random() * 4) + 1}`, 250);
+				await writeOn(
+					context,
+					`Rolled a ${Math.floor(Math.random() * 4) + 1}`,
+					250
+				);
 				shimmer();
 			},
 			logger: this.logger
@@ -164,10 +199,14 @@ export class RpgmForge extends FoundyRpgmModuleMixin<typeof AbstractForge, Abstr
 			category: rpgm.radialMenu.categories.rpgm_forge,
 			icon: 'fa fa-dice-d6',
 			tooltip: 'RPGM_FORGE.RADIAL_MENU.D6',
-			detective: (context) => inputHeuristics(context).noNumber().result,
-			callback: async (context) => {
+			detective: context => inputHeuristics(context).noNumber().result,
+			callback: async context => {
 				const shimmer = shimmerInput(context);
-				await writeOn(context, `Rolled a ${Math.floor(Math.random() * 6) + 1}`, 250);
+				await writeOn(
+					context,
+					`Rolled a ${Math.floor(Math.random() * 6) + 1}`,
+					250
+				);
 				shimmer();
 			},
 			logger: this.logger
@@ -176,12 +215,15 @@ export class RpgmForge extends FoundyRpgmModuleMixin<typeof AbstractForge, Abstr
 			category: rpgm.radialMenu.categories.rpgm_forge,
 			icon: 'fa fa-signature',
 			tooltip: 'RPGM_FORGE.RADIAL_MENU.NAMES',
-			callback: async (context) => {
-				if (!context.token) return rpgm.forge.logger.visible.log('No token selected');
+			callback: async context => {
+				if (!context.token)
+					return rpgm.forge.logger.visible.log('No token selected');
 				if (context.shift)
-					void chatTokenNames(context.token, context.token.actor?.prototypeToken?.name);
-				else
-					return quickNameToken(context.token.document);
+					void chatTokenNames(
+						context.token,
+						context.token.actor?.prototypeToken?.name
+					);
+				else return quickNameToken(context.token.document);
 			},
 			logger: this.logger
 		});
@@ -189,13 +231,17 @@ export class RpgmForge extends FoundyRpgmModuleMixin<typeof AbstractForge, Abstr
 			category: rpgm.radialMenu.categories.rpgm_forge,
 			icon: 'fa fa-align-left',
 			tooltip: 'RPGM_FORGE.RADIAL_MENU.DESCRIPTION',
-			callback: (context) => {
+			callback: context => {
 				const token = context.token;
 				if (!token) return rpgm.forge.logger.log('No token selected');
 				chatDescription({
 					type: token.actor!.prototypeToken.name,
 					// Don't include name if it's the default actor name
-					name: token.name ? token.name !== token.actor!.prototypeToken.name ? token.name : '' : ''
+					name: token.name
+						? token.name !== token.actor!.prototypeToken.name
+							? token.name
+							: ''
+						: ''
 				});
 			},
 			logger: this.logger
@@ -212,8 +258,14 @@ export class RpgmForge extends FoundyRpgmModuleMixin<typeof AbstractForge, Abstr
 			component: ForgeSidebar
 		});
 		registerTokenCreate();
-		rpgm.help.registerHelpTopic('Getting Started With Forge', HelpGettingStarted);
-		rpgm.help.registerHelpTopic('Forge Configuration Options', HelpForgeConfigurationOptions);
+		rpgm.help.registerHelpTopic(
+			'Getting Started With Forge',
+			HelpGettingStarted
+		);
+		rpgm.help.registerHelpTopic(
+			'Forge Configuration Options',
+			HelpForgeConfigurationOptions
+		);
 		rpgm.help.registerHelpTopic('Using Forge', HelpUsingForge);
 		rpgm.help.registerHelpTopic('Forge FAQ', HelpForgeFAQ);
 		rpgm.help.registerHelpTopic('Troubleshooting Forge', HelpTroubleshooting);
@@ -221,7 +273,10 @@ export class RpgmForge extends FoundyRpgmModuleMixin<typeof AbstractForge, Abstr
 	}
 
 	override ready() {
-		if (this.promptChats.data.size === 0 && !game.settings.get('rpgm-forge', 'has_been_prompted')) {
+		if (
+			this.promptChats.data.size === 0 &&
+			!game.settings.get('rpgm-forge', 'has_been_prompted')
+		) {
 			this.promptChats.newMessage({});
 		}
 	}
@@ -231,14 +286,13 @@ export class RpgmForge extends FoundyRpgmModuleMixin<typeof AbstractForge, Abstr
 		const textLimit = ref(0);
 		if (session.value) {
 			const limit = await this.getApiForgeUsage();
-			if (typeof limit.data === 'number')
-				textLimit.value = limit.data;
+			if (typeof limit.data === 'number') textLimit.value = limit.data;
 		}
-		const decrement = () => textLimit.value = Math.max(0, textLimit.value - 1);
+		const decrement = () =>
+			(textLimit.value = Math.max(0, textLimit.value - 1));
 		const update = async () => {
 			const limit = await this.getApiForgeUsage();
-			if (typeof limit.data === 'number')
-				textLimit.value = limit.data;
+			if (typeof limit.data === 'number') textLimit.value = limit.data;
 			return textLimit.value;
 		};
 		const textLimitValue = computed(() => {
@@ -254,21 +308,25 @@ export class RpgmForge extends FoundyRpgmModuleMixin<typeof AbstractForge, Abstr
 			content: rpgm.localize('RPGM_FORGE.ERRORS.TEXT_LIMIT'),
 			duration: 10000,
 			action() {
-				return h(NButton, {
-					type: 'warning',
-					onClick: () => {
-						n.destroy();
-						// eslint-disable-next-line @typescript-eslint/no-explicit-any
-						(ui.rpgm as any).router.push('/guildhall');
-						if (rpgm.majorGameVersion <= 12) {
-							ui.sidebar.activateTab('rpgm');
-						} else {
-							ui.sidebar.changeTab('rpgm', 'primary');
+				return h(
+					NButton,
+					{
+						type: 'warning',
+						onClick: () => {
+							n.destroy();
+							// eslint-disable-next-line @typescript-eslint/no-explicit-any
+							(ui.rpgm as any).router.push('/guildhall');
+							if (rpgm.majorGameVersion <= 12) {
+								ui.sidebar.activateTab('rpgm');
+							} else {
+								ui.sidebar.changeTab('rpgm', 'primary');
+							}
 						}
+					},
+					{
+						default: () => 'Upgrade Your Membership'
 					}
-				}, {
-					default: () => 'Upgrade Your Membership'
-				});
+				);
 			},
 			type: 'warning'
 		});
