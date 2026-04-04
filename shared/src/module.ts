268d4c3906ed697c2d5ee7909e95f5fe8f9b576e
Aaron Westover <61663569+neostryder@users.noreply.github.com>
2026-04-04 13:37:52 -0700

    The Fix Awakens, part zero

--- shared/src/module.ts
+++ shared/src/module.ts
@@ -1,5 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import type { AbstractRpgmModule, AbstractTools, IRpgmModule, ModuleMap } from '@rpgm/tools';
+import type {
+	AbstractRpgmModule,
+	AbstractTools,
+	IRpgmModule,
+	ModuleMap
+} from '@rpgm/tools';
 import { RpgmLogger } from '@rpgm/tools';
 
 import { LocalStorageMap } from './settings';
@@ -8,17 +13,30 @@ import { RpgmTools } from './tools';
 type VoidPromise = void | Promise<void>;
 
 type AbstractConstructor<T> = abstract new (...args: any[]) => T;
-export type FoundryRpgmModule = InstanceType<ReturnType<typeof FoundyRpgmModuleMixin>>;
+export type FoundryRpgmModule = InstanceType<
+	ReturnType<typeof FoundyRpgmModuleMixin>
+>;
 
-export function FoundyRpgmModuleMixin<C extends AbstractConstructor<AbstractRpgmModule<Settings> & IRpgmModule<keyof ModuleMap, Settings>>, Settings extends AbstractRpgmModule.ModuleSettings>(Base: C) {
+export function FoundyRpgmModuleMixin<
+	C extends AbstractConstructor<
+		AbstractRpgmModule<Settings> & IRpgmModule<keyof ModuleMap, Settings>
+	>,
+	Settings extends AbstractRpgmModule.ModuleSettings
+>(Base: C) {
 	abstract class FoundryRpgmModule extends Base {
 		readonly version = __MODULE_VERSION__;
 
 		private static show(method: 'log' | 'warn' | 'error', message: string) {
-			const notif = ui.notifications?.[method === 'log' ? 'info' : method](message, { console: false }) as number | Notifications.Notification;
+			const notif = ui.notifications?.[
+				method === 'log' ? 'info' : method
+			](message, { console: false }) as
+				| number
+				| Notifications.Notification;
 			if (typeof notif === 'number') {
 				// Foundry V12
-				(ui.notifications as any).element.find(`[data-id="${notif}"]`).addClass('rpgm');
+				(ui.notifications as any).element
+					.find(`[data-id="${notif}"]`)
+					.addClass('rpgm');
 			} else {
 				if (notif.element) {
 					notif.element.classList.add('rpgm');
@@ -30,44 +48,59 @@ export function FoundyRpgmModuleMixin<C extends AbstractConstructor<AbstractRpgm
 
 		override logger: RpgmLogger;
 
-		override settings = new LocalStorageMap<Settings>(this.id, this.DEFAULT_SETTINGS);
+		override settings = new LocalStorageMap<Settings>(
+			this.id,
+			this.DEFAULT_SETTINGS
+		);
 
-		protected override get tools() { return window.rpgm as unknown as AbstractTools; };
+		protected override get tools() {
+			return window.rpgm as unknown as AbstractTools;
+		}
 
 		protected bootstrap = Promise.resolve();
 
 		constructor(..._args: any[]) {
 			super();
-			Hooks.once('init', () => { this.bootstrap = this.bootstrap.then(this._init.bind(this)); });
-			Hooks.once('setup', () => { this.bootstrap = this.bootstrap.then(this._setup.bind(this)); });
-			Hooks.once('ready', () => { this.bootstrap = this.bootstrap.then(this._ready.bind(this)); });
+			Hooks.once('init', () => {
+				this.bootstrap = this.bootstrap.then(this._init.bind(this));
+			});
+			Hooks.once('setup', () => {
+				this.bootstrap = this.bootstrap.then(this._setup.bind(this));
+			});
+			Hooks.once('ready', () => {
+				this.bootstrap = this.bootstrap.then(this._ready.bind(this));
+			});
 		}
 
 		private async _init() {
-			this.logger = RpgmLogger.fromModule(this, { show: FoundryRpgmModule.show });
+			this.logger = RpgmLogger.fromModule(this, {
+				show: FoundryRpgmModule.show
+			});
 			if (!window.rpgm) {
 				this.logger.debug('first');
 				window.rpgm = new RpgmTools();
 				rpgm._init();
 			}
-			this.logger.prefixed('').log(`${this.icon} ${this.name} joined the game`);
+			this.logger
+				.prefixed('')
+				.log(`${this.icon} ${this.name} joined the game`);
 			rpgm.modules[this.id] = this as any;
 			await this.init();
 		}
 
-		protected init(): VoidPromise { }
+		protected init(): VoidPromise {}
 
 		private async _setup() {
 			this.registerSettings();
 		}
 
-		protected registerSettings(): VoidPromise { }
+		protected registerSettings(): VoidPromise {}
 
 		private async _ready() {
 			await this.ready();
 		}
 
-		protected ready(): VoidPromise { }
+		protected ready(): VoidPromise {}
 
 		override save(data: unknown): void {
 			const dataString = JSON.stringify(data);
@@ -76,7 +109,9 @@ export function FoundyRpgmModuleMixin<C extends AbstractConstructor<AbstractRpgm
 
 		override load() {
 			try {
-				return JSON.parse(localStorage.getItem(`${this.id}.settings`) ?? 'null');
+				return JSON.parse(
+					localStorage.getItem(`${this.id}.settings`) ?? 'null'
+				);
 			} catch {
 				this.logger.error('Failed to load settings');
 				return null;
